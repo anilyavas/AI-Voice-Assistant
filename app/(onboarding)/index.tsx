d3baff26623a32784494a1b7154a2a986bc72e2c
anilyavas <anil.yavas99@gmail.com>
2024-11-25 18:20:54 +0300

    linear gradient

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, useWindowDimensions } from 'react-native';
 
 export default function OnboardingScreen() {
+  const { height } = useWindowDimensions();
   return (
     <View>
-      <Text>Hello screen 1</Text>
+      <LinearGradient
+        colors={['#250252', '#000000']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={{ top: 0, left: 0, right: 0, height: height }}
+      >
+        <Text>Hello screen 1</Text>
+        <StatusBar style='light' />
+      </LinearGradient>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import '../global.css';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name='(onboarding)' />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='(onboarding)/index' />
       <Stack.Screen name='index' />
     </Stack>
   );
