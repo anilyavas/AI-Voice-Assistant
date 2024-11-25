b123e42a69f6cb061ec36862b608007c5a4ca6b4
anilyavas <anil.yavas99@gmail.com>
2024-11-25 18:45:55 +0300

    onboarding screen image

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,20 +1,28 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, useWindowDimensions } from 'react-native';
+import Onboarding1 from '../../assets/svgs/onboarding1';
 
 export default function OnboardingScreen() {
   const { height } = useWindowDimensions();
   return (
-    <View>
-      <LinearGradient
-        colors={['#250252', '#000000']}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        style={{ top: 0, left: 0, right: 0, height: height }}
-      >
-        <Text>Hello screen 1</Text>
-        <StatusBar style='light' />
-      </LinearGradient>
-    </View>
+    <LinearGradient
+      colors={['#250252', '#000000']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={{
+        top: 0,
+        left: 0,
+        right: 0,
+        height: height,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+      }}
+    >
+      <StatusBar style='light' />
+      <Onboarding1 />
+      <Text></Text>
+    </LinearGradient>
   );
 }
---
+++ constants/constants.tsx
@@ -0,0 +1,21 @@
+import Onboarding1 from '../assets/svgs/onboarding1';
+export const onBoardingData = [
+  {
+    id: 1,
+    title: '',
+    subtitle: '',
+    image: <Onboarding1 />,
+  },
+  {
+    id: 2,
+    title: '',
+    subtitle: '',
+    image: <Onboarding1 />,
+  },
+  {
+    id: 3,
+    title: '',
+    subtitle: '',
+    image: <Onboarding1 />,
+  },
+];
